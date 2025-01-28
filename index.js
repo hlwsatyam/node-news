a2e618c9f2f2fec5b8dc2391f413be25e0c1ed67
hlwsatyam <satyampandit021@gmail.com>
2025-01-28 18:17:55 +0530

    dfdf

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-
+import cors   from "cors";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
 import commentRoutes from "./routes/comment.route.js"
 
 dotenv.config()
+app.use(cors());
 
 mongoose
   .connect("mongodb+srv://satyampandit021:20172522@rvbmhotelbooking.9hfzkrx.mongodb.net/news?retryWrites=true&w=majority")
@@ -29,7 +30,7 @@ app.listen(5000, () => {
   console.log("Server is running on port 5000!")
 })
 app.get("/", (req, res) => {
-  res.send("Hello news World!")
+  res.send("Hello l World!")
 })
 app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
